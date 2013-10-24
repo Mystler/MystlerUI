f9b53191772f4ff963401538f211e0ee6fa52300
Florian Meißner <developer@mystler.eu>
2013-10-24 23:04:39 +0200

    Add completely dramatic death sound

--- MystlerUI.lua
+++ MystlerUI.lua
@@ -3,3 +3,15 @@ LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar01", [[Interface\Addons\Mystler
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar02", [[Interface\Addons\MystlerUI\tex\MystlerBar02]])
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar03", [[Interface\Addons\MystlerUI\tex\MystlerBar03]])
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar04", [[Interface\Addons\MystlerUI\tex\MystlerBar04]])
+
+-- Event Handling
+local frame, events = CreateFrame("Frame"), {};
+function events:PLAYER_DEAD(...)
+    PlaySoundFile([[Interface\Addons\MystlerUI\sfx\defeat.ogg]], "Master")
+end
+
+-- Event Dispatcher
+frame:SetScript("OnEvent", function(self, event, ...) events[event](self, ...); end);
+for k, v in pairs(events) do
+    frame:RegisterEvent(k);
+end
