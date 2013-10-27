c3cc531840aa1d00cb913dbe354fa03478ac1b9c
Florian Meißner <developer@mystler.eu>
2013-10-27 10:18:47 +0100

    Use Ace3 library Add license Fix multiple poison alerts

--- MystlerUI.lua
+++ MystlerUI.lua
@@ -1,40 +1,54 @@
+-- AddOn setup
+MystlerUI = LibStub("AceAddon-3.0"):NewAddon("MystlerUI", "AceEvent-3.0");
+
+-- Register new textures with LibSharedMedia
 LSM = LibStub("LibSharedMedia-3.0");
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar01", [[Interface\Addons\MystlerUI\tex\MystlerBar01]]);
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar02", [[Interface\Addons\MystlerUI\tex\MystlerBar02]]);
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar03", [[Interface\Addons\MystlerUI\tex\MystlerBar03]]);
 LSM:Register(LSM.MediaType.STATUSBAR, "MystlerBar04", [[Interface\Addons\MystlerUI\tex\MystlerBar04]]);
 
--- Rogue Poison Alert
-local function PoisonAlert()
-    if not UnitBuff("player", "Deadly Poison") and
-            not UnitBuff("player", "Wound Poison") then
-        PlaySoundFile([[Interface\Addons\MystlerUI\sfx\poison.ogg]], "Master");
-        return;
-    end
-    if not UnitBuff("player", "Leeching Poison") and
+function MystlerUI:OnInitialize()
+    -- Initialize member variables
+    MystlerUI.PoisonOK = true;
+end
+
+function MystlerUI:OnEnable()
+    -- Register events
+    MystlerUI:RegisterEvent("PLAYER_DEAD");
+    MystlerUI:RegisterEvent("UNIT_AURA");
+end
+
+function MystlerUI:OnDisable()
+    -- Unregister events
+    MystlerUI:UnregisterAllEvents();
+end
+
+-- Rogue: Poison alert sound check
+function MystlerUI:PoisonAlert()
+    if (not UnitBuff("player", "Deadly Poison") and
+            not UnitBuff("player", "Wound Poison")) or
+            (not UnitBuff("player", "Leeching Poison") and
             not UnitBuff("player", "Crippling Poison") and
-            not UnitBuff("player", "Mind-numbing Poison") then
-        PlaySoundFile([[Interface\Addons\MystlerUI\sfx\poison.ogg]], "Master");
-        return;
+            not UnitBuff("player", "Mind-numbing Poison")) then
+        if MystlerUI.PoisonOK then
+            PlaySoundFile([[Interface\Addons\MystlerUI\sfx\poison.ogg]], "Master");
+        end
+        MystlerUI.PoisonOK = false;
+    else
+        MystlerUI.PoisonOK = true;
     end
 end
 
--- Event Handling
-local frame, events = CreateFrame("Frame"), {};
-function events:PLAYER_DEAD(...)
+-- Events
+function MystlerUI:PLAYER_DEAD()
     PlaySoundFile([[Interface\Addons\MystlerUI\sfx\defeat.ogg]], "Master");
 end
-function events:UNIT_AURA(...)
-    local unit = ...;
+
+function MystlerUI:UNIT_AURA(event, unit)
     if unit == "player" then
         if UnitClass("player") == "Rogue" then
-            PoisonAlert();
+            MystlerUI:PoisonAlert();
         end
     end
-end
-
--- Event Dispatcher
-frame:SetScript("OnEvent", function(self, event, ...) events[event](self, ...); end);
-for k, v in pairs(events) do
-    frame:RegisterEvent(k);
 end
---
+++ README.md
@@ -0,0 +1,12 @@
+MystlerUI
+=========
+
+This AddOn contains personal tweaks and improvements that I use in my World of Warcraft UI configuration.
+
+Features
+--------
+
+* Custom statusbar textures that can be used in LibSharedMedia AddOns like Recount, Grid, ...
+* Custom sound effects
+  * *Dun dun daaaah* on death
+  * *Poison alert!* on missing poisons (rogue character)
